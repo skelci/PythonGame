2fb945c953ec965e3a93c9682dab1674afeabff2
Skelci <nikomaren2@gmail.com>
2025-02-05 17:28:43 +0100

    started on character component

---
+++ src/Components/character.py
@@ -0,0 +1,85 @@
+from components.rigidbody import Rigidbody
+
+from components.datatypes import *
+
+
+
+class Character(Rigidbody):
+    def __init__(self, name, half_size, position = Vector(), visible = True, texture = None, restitution = 0.01, initial_velocity = Vector(), min_velocity = kinda_small_number, mass = 100, gravity_scale = 1, friction = 8, air_resistance = 0.1, jump_velocity = 10, walk_speed = 5, acceleration = 1, air_control = 0.5):
+        super().__init__(name, half_size, position, visible, texture, restitution, initial_velocity, min_velocity, mass, gravity_scale, friction, air_resistance)
+        
+        self.jump_velocity = jump_velocity
+        self.walk_speed = walk_speed
+        self.acceleration = acceleration
+        self.air_control = air_control
+
+
+    @property
+    def jump_velocity(self):
+        return self.__jump_velocity
+    
+
+    @jump_velocity.setter
+    def jump_velocity(self, value):
+        if isinstance(value, (int, float)) and value >= 0:
+            self.__jump_velocity = value
+        else:
+            raise Exception("Jump velocity must be a positive float:", value)
+        
+
+    @property
+    def walk_speed(self):
+        return self.__walk_speed
+    
+
+    @walk_speed.setter
+    def walk_speed(self, value):
+        if isinstance(value, (int, float)) and value >= 0:
+            self.__walk_speed = value
+        else:
+            raise Exception("Walk speed must be a positive float:", value)
+        
+
+    @property
+    def acceleration(self):
+        return self.__acceleration
+    
+
+    @acceleration.setter
+    def acceleration(self, value):
+        if isinstance(value, (int, float)) and value >= 0:
+            self.__acceleration = value
+        else:
+            raise Exception("Acceleration must be a positive float:", value)
+        
+
+    @property
+    def air_control(self):
+        return self.__air_control
+    
+
+    @air_control.setter
+    def air_control(self, value):
+        if isinstance(value, (int, float)) and value >= 0:
+            self.__air_control = value
+        else:
+            raise Exception("Air control must be a positive float:", value)
+        
+
+    @property
+    def is_grounded(self):
+        return self.collided_sides.y > 0
+        
+
+    def move(self, direction, delta_time):
+        if self.is_grounded:
+            self.velocity.x += direction * self.acceleration * delta_time
+        else:
+            self.velocity.x += direction * self.acceleration * self.air_control * delta_time
+
+
+    def jump(self):
+        if self.is_grounded:
+            self.velocity.y = self.jump_velocity
+            
+
