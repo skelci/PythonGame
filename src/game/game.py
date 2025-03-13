69ed9307669ebe24049bf02818c64183bccb0f5c
Berzi <matevz.ber2008@gmail.com>
2025-03-13 18:21:37 +0100

    game testing

--- src/game/game.py
+++ src/game/game.py
@@ -14,9 +14,13 @@ import random as r
 #TODO change every isubclass to isinstance
 class Grass(Actor):
     def __init__(self, engine_ref, name, position):
-        super().__init__(engine_ref, name, position = position, half_size = Vector(0.5, 0.5), material = Material(Color(0, 255, 0)))
+        super().__init__(engine_ref, name, position = position, half_size = Vector(0.5, 0.5), material = Material("res/textures/grass.jpeg"))
         self.position = position
 
+class Dirt(Actor):
+    def __init__(self, engine_ref, name, position):
+        super().__init__(engine_ref, name, position = position, half_size = Vector(0.5, 0.5), material = Material(Color(139, 69, 19)))
+        self.position = position
 
 class TestPlayer(Character):
     def __init__(self, engine_ref, name):
@@ -40,7 +44,7 @@ class ClientGame(ClientGameBase):
 
         self.engine.add_actor_template(TestPlayer)
         self.engine.add_actor_template(Grass)
-
+        self.engine.add_actor_template(Dirt)
 
     def tick(self):
         delta_time = super().tick()
@@ -66,10 +70,36 @@ class ClientGame(ClientGameBase):
 class TestLevel(Level):
     def __init__(self, engine_ref):
         actors = (
-            Grass(engine_ref, "Grass", Vector(0,-1)),
-            Grass(engine_ref, "Grass_1", Vector(2,-1)),
+            Grass(engine_ref, "Grass", Vector(-24,-1)),
+            Grass(engine_ref, "Grass_1", Vector(-23,-1)),
+            Grass(engine_ref, "Grass_2", Vector(-22,-1)),
+            Grass(engine_ref, "Grass_3", Vector(-21,-1)),
+            Grass(engine_ref, "Grass_4", Vector(-20,-1)),
+            Grass(engine_ref, "Grass_5", Vector(-19,-1)),
+            Grass(engine_ref, "Grass_6", Vector(-18,-1)),
+            Grass(engine_ref, "Grass_7", Vector(-17,-1)),
+            Grass(engine_ref, "Grass_8", Vector(-16,-1)),
+            Grass(engine_ref, "Grass_9", Vector(-15,-1)),
+            Grass(engine_ref, "Grass_10", Vector(-14,-1)),
+            Grass(engine_ref, "Grass_11", Vector(-13,-1)),
+            Grass(engine_ref, "Grass_12", Vector(-12,-1)),
+            Grass(engine_ref, "Grass_13", Vector(-11,-1)),
+            Grass(engine_ref, "Grass_14", Vector(-10,-1)),
+            Grass(engine_ref, "Grass_15", Vector(-9,-1)),
+            Grass(engine_ref, "Grass_16", Vector(-8,-1)),
+            Grass(engine_ref, "Grass_17", Vector(-7,-1)),
+            Grass(engine_ref, "Grass_18", Vector(-6,-1)),
+            Grass(engine_ref, "Grass_19", Vector(-5,-1)),
+            Grass(engine_ref, "Grass_20", Vector(-4,-1)),
+            Grass(engine_ref, "Grass_21", Vector(-3,-1)),
+            Grass(engine_ref, "Grass_22", Vector(-2,-1)),
+            Grass(engine_ref, "Grass_23", Vector(-1,-1)),
+            Grass(engine_ref, "Grass_24", Vector(0,-1)),
 
-            
+            Dirt(engine_ref, "Dirt", Vector(0,-2)),
+            Dirt(engine_ref, "Dirt_1", Vector(1,-2)),
+            Dirt(engine_ref, "Dirt_2", Vector(2,-2)),
+    
         )
 
         backgrounds = (
