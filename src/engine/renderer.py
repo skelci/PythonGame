74d8f5cb5b78ae0ef738b94097ea0653398543e3
Skelci <nikomaren2@gmail.com>
2025-04-08 12:39:03 +0200

    added documentation for render class

--- src/engine/renderer.py
+++ src/engine/renderer.py
@@ -1,8 +1,13 @@
 #?attr CLIENT
 
+"""
+Renderer class for managing the Pygame display and rendering.
+"""
+
 from components.datatypes import *
 from components.actor import Actor
 from components.widget import Widget
+from components.background import Background
 
 import pygame
 
@@ -11,7 +16,25 @@ import time
 
 
 class Renderer:
-    def __init__(self, width, height, camera_width, title = "Pygame Window", fullscreen = False, windowed = True, camera_position = Vector()):
+    """
+    Renderer class for managing the Pygame display and rendering.
+    This class handles everything related to the display.
+    """
+
+
+    def __init__(self, width: int, height: int, camera_width: float, title = "Pygame Window", fullscreen = False, windowed = True, camera_position = Vector()):
+        """
+        Args:
+            width (int): Width of the window.
+            height (int): Height of the window.
+            camera_width (float): Width of the camera in game world units.
+            title (str): Title of the window.
+            fullscreen (bool): If True, the display is in fullscreen mode.
+            windowed (bool): If True, the display is in windowed mode.
+            camera_position (Vector): Position of the camera in the game world.
+        Raises:
+            TypeError: If any of the arguments are of incorrect type.
+        """
         self.__screen = None
         self.__fullscreen = False
         self.__windowed = True
@@ -29,6 +52,9 @@ class Renderer:
 
     @property
     def resolution(self):
+        """
+        Vector - The window dimensions represented as a vector (width, height). This value is used to set the display mode.
+        """
         return self.__resolution
 
 
@@ -46,6 +72,9 @@ class Renderer:
 
     @property
     def title(self):
+        """
+        str - The title of the window. This value is used to set the window caption.
+        """
         return self.__title
     
 
@@ -60,6 +89,9 @@ class Renderer:
 
     @property
     def camera_position(self):
+        """
+        Vector - The position of the camera in the game world. This value is used to determine the rendering offset.
+        """
         return self.__camera_position
     
 
@@ -73,6 +105,9 @@ class Renderer:
 
     @property
     def camera_width(self):
+        """
+        float - The width of the camera in game world units. This value is used to scale the rendering of actors and widgets.
+        """
         return self.__camera_width
     
 
@@ -86,6 +121,10 @@ class Renderer:
 
     @property
     def fullscreen(self):
+        """
+        bool - Indicates whether the display is in fullscreen mode. If True, the display occupies the entire screen.
+        It also overrides resolution and windowed settings.
+        """
         return self.__fullscreen
     
 
@@ -93,6 +132,7 @@ class Renderer:
     def fullscreen(self, value):
         if isinstance(value, bool):
             self.__fullscreen = value
+            self.__resolution = Vector(pygame.display.Info().current_w, pygame.display.Info().current_h) if value else self.resolution
             self.__screen = pygame.display.set_mode((self.resolution.x, self.resolution.y),
                                                     pygame.FULLSCREEN if value else 0 |
                                                     pygame.NOFRAME if not self.windowed else 0 |
@@ -103,6 +143,9 @@ class Renderer:
 
     @property
     def windowed(self):
+        """
+        bool - Indicates whether the display is in windowed mode. If True, the window header is shown.
+        """
         return self.__windowed
     
 
@@ -120,39 +163,72 @@ class Renderer:
 
     @property
     def screen(self):
+        """
+        pygame.display - The Pygame display surface.
+        """
         return self.__screen
     
 
     @property
     def actors_to_draw(self):
+        """
+        dict[int, set[Actor]] - Dictionary of actors to draw, where key is the render layer and value is a set of actors.
+        """
         return self.__actors_to_draw
     
 
     @property
     def widgets_to_draw(self):
+        """
+        Returns:
+            list[Widget] - List of widgets to draw for current frame.
+        """
         return self.__widgets_to_draw
 
 
-    def add_actor_to_draw(self, actor):
+    def add_actor_to_draw(self, actor: Actor):
+        """
+        Called only inside engine.
+        It adds actor to the list of actors to draw.\n
+        Args:
+            actor (Actor): Actor to add to the list of actors to draw.
+        Raises:
+            TypeError: If actor is not a subclass of Actor.
+        """
         if not isinstance(actor, Actor):
             raise TypeError("Actor must be a subclass of Actor:", actor)
-        
         if actor.render_layer not in self.__actors_to_draw:
             self.__actors_to_draw[actor.render_layer] = set()
         self.__actors_to_draw[actor.render_layer].add(actor)
 
 
-    def remove_actor_from_draw(self, actor):
+    def remove_actor_from_draw(self, actor: Actor):
+        """
+        Called only inside engine.
+        It removes actor from the list of actors to draw.\n
+        Args:
+            actor (Actor): Actor to remove from the list of actors to draw.
+        Raises:
+            TypeError: If actor is not a subclass of Actor.
+            ValueError: If actor is not found in the list of actors to draw.
+        """
         if not isinstance(actor, Actor):
             raise TypeError("Actor must be a subclass of Actor:", actor)
-        
         if actor in self.__actors_to_draw[actor.render_layer]:
             self.__actors_to_draw[actor.render_layer].remove(actor)
         else:
             raise ValueError("Actor not found in actors to draw:", actor)
         
 
-    def add_widget_to_draw(self, widget):
+    def add_widget_to_draw(self, widget: Widget):
+        """
+        Called only inside engine before rendering begins.
+        It adds widget to the list of widgets to draw for current frame.\n
+        Args:
+            widget (Widget): Widget to add to the list of widgets to draw.
+        Raises:
+            TypeError: If widget is not a subclass of Widget.
+        """
         if not isinstance(widget, Widget):
             raise TypeError("Widget must be a subclass of Widget:", widget)
         
@@ -160,10 +236,20 @@ class Renderer:
 
     
     def clear(self):
+        """
+        Called only inside engine at the beginning of the frame.
+        This method clears buffers from previous frame.
+        """
         self.__widgets_to_draw.clear()
 
 
     def render(self):
+        """
+        Called only inside engine after background is drawn.
+        It draws actors and widgets.\n
+        Returns:
+            tuple[time_actors, time_widgets] - Time taken to draw actors and widgets respectively.
+        """
         camera_ratio = self.resolution.x / self.camera_width
         
         time_start = time.time()
@@ -194,7 +280,11 @@ class Renderer:
         return (time_actors - time_start, time_widgets - time_actors)
 
 
-    def draw_background(self, background):
+    def draw_background(self, background: Background):
+        """
+        Called only inside engine at the beginning of the frame after buffers are cleared.
+        It draws background.
+        """
         bg_surface = background.get_bg_surface(self.camera_position, self.resolution, self.camera_width)
         self.screen.blit(bg_surface, (0, 0))
 
